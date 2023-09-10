78b8010ee992676d1186c6c689e8f6b442b0ee7a
Navid <navidjalili3@gmail.com>
2023-09-10 22:39:16 +0330

    Implement an asynchronous connectivity checker function

--- src/checker.py
+++ src/checker.py
@@ -1,3 +1,5 @@
+import asyncio
+import aiohttp
 from http.client import HTTPConnection
 from urllib.parse import urlparse
 
@@ -19,4 +21,25 @@ def site_is_online(url: str, timeout=2) -> bool:
             error = e
         finally:
             connection.close()
+    raise error
+
+
+async def site_is_online_async(url, timeout=2):
+    """Return True if the target URL is online.
+
+    Raise an exception otherwise.
+    """
+    error = Exception("unknown error")
+    parser = urlparse(url)
+    host = parser.netloc or parser.path.split("/")[0]
+    for scheme in ("http", "https"):
+        target_url = scheme + "://" + host
+        async with aiohttp.ClientSession() as session:
+            try:
+                await session.head(target_url, timeout=timeout)
+                return True
+            except asyncio.exceptions.TimeoutError:
+                error = Exception("timed out")
+            except Exception as e:
+                error = e
     raise error
